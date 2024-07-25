12af3a38fbf219fcfd0e8bcbe5adf5eb9c72ce16
Nick Liu <nicku5011@gmail.com>
2024-07-25 22:02:55 +0800

    update fare 20240714

--- data/TaxiFareRule.js
+++ data/TaxiFareRule.js
@@ -28,7 +28,8 @@ function createTaxiFareRule({
     }
 }
 
-const HongKongTaxiFareRule = {
+// Before 2024-07-14
+const HongKongTaxiFareRule_old = {
     UrbanTaxi: createTaxiFareRule({
         initial2KMFare: 27.0,
         subsequent200mOr1MinuteFare: 1.9,
@@ -68,5 +69,48 @@ const HongKongTaxiFareRule = {
         perCrossHarbourTunnelDoubleTripFare: null,  // Lantau Taxi cannot cross tunnel
     })
 }
+
+const HongKongTaxiFareRule_20240714 = {
+    UrbanTaxi: createTaxiFareRule({
+        initial2KMFare: 29.0,
+        subsequent200mOr1MinuteFare: 2.1,
+        subsequent200mOr1MinuteFareDiscounted: 1.4,
+        fareDiscountedThreshold: 102.5,
+        perPackageFare: 6.0,
+        perWheelChairFare: 0.0,
+        perAnimalFare: 5.0,
+        perCallServiceFare: 5.0,
+        perCrossHarbourTunnelSingleTripFare: 25.0,
+        perCrossHarbourTunnelDoubleTripFare: 50.0,
+    }),
+
+    NewTerritoriesTaxi: createTaxiFareRule({
+        initial2KMFare: 25.5,
+        subsequent200mOr1MinuteFare: 1.9,
+        subsequent200mOr1MinuteFareDiscounted: 1.4,
+        fareDiscountedThreshold: 82.5,
+        perPackageFare: 6.0,
+        perWheelChairFare: 0.0,
+        perAnimalFare: 5.0,
+        perCallServiceFare: 5.0,
+        perCrossHarbourTunnelSingleTripFare: null,  // New Territories Taxi cannot cross tunnel
+        perCrossHarbourTunnelDoubleTripFare: null,  // New Territories Taxi cannot cross tunnel
+    }),
+
+    LantauTaxi: createTaxiFareRule({
+        initial2KMFare: 24.0,
+        subsequent200mOr1MinuteFare: 1.9,
+        subsequent200mOr1MinuteFareDiscounted: 1.6,
+        fareDiscountedThreshold: 195.0,
+        perPackageFare: 6.0,
+        perWheelChairFare: 0.0,
+        perAnimalFare: 5.0,
+        perCallServiceFare: 5.0,
+        perCrossHarbourTunnelSingleTripFare: null,  // Lantau Taxi cannot cross tunnel
+        perCrossHarbourTunnelDoubleTripFare: null,  // Lantau Taxi cannot cross tunnel
+    })
+}
+
+const HongKongTaxiFareRule = HongKongTaxiFareRule_20240714
 
 export default HongKongTaxiFareRule
